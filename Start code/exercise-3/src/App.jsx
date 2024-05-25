1ee2e579b622ad124efcba47bea530b9ea1fc4e9
Mixchennie0505 <mixchennie@gmail.com>
2024-05-25 09:55:02 +0700

    exercise-3

--- Start code/exercise-3/src/App.jsx
+++ Start code/exercise-3/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { ALL_PNV_TEACHERS } from "./teachers.js";
-
 // NO change to perform here...
 export function User({ user }) {
   return (
     <div id="user" data-testid="user">
       <h2>
-        {user.firstName} {user.lastName}{" "}
+        {user.firstName} {user.lastName}
       </h2>
-      <p> {user.title}</p>
+      <p>{user.title}</p>
     </div>
   );
 }
@@ -18,7 +17,9 @@ function App() {
     <div id="app">
       <h1>PNV Team !!</h1>
       <p>Here are some PNV trainers and educators, do you know them?</p>
-      <User /> {/* How to loop on ALL_PNV_TEACHERS list ? */}
+      {ALL_PNV_TEACHERS.map((teacher, index) => (
+        <User key={index} user={teacher} />
+      ))}
     </div>
   );
 }
